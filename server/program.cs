67d30839d39e233dea3eddd6dc5855a2bedbe9ca
Sofia1-min <sofiahortunova@gmail.com>
2026-09-24 13:25:05 +0300

    Коммит 9: добавление меню

--- server/program.cs
+++ server/program.cs
@@ -7,4 +7,37 @@ Console.WriteLine("1 — Показать ФИО");
 Console.WriteLine("2 — Показать группу");
  Console.WriteLine("3 — Показать дату");
 Console.WriteLine("4 — Выход");
- 
+string? a = Console.ReadLine();
+while (true)
+{
+    if (a == "1")
+
+    {
+        Console.WriteLine("Хортюнова София Юрьевна");
+        break;
+    }
+    else if (a == "2")
+
+    {
+        Console.WriteLine("ИСП-241");
+        break;
+    }
+    else if (a == "3")
+
+    {
+        Console.WriteLine("24.09.2026");
+        break;
+    }
+    else if (a == "4")
+    {
+
+        break;
+    }
+    else
+
+    {
+        Console.WriteLine("Неверный выбор");
+        break;
+    }
+
+}
